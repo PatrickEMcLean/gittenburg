625e37c92d13a68660df8401d0d2d527a8abe2ad
PatrickEMcLean <patrickemclean@gmail.com>
2013-01-15 12:41:15 -0500

    fixed build system       76 total

--- build/build.sh
+++ build/build.sh
@@ -6,22 +6,22 @@
 # 
 
 
+
 if [ -f ../output/fullbook.md ];
 then
    rm ../output/fullbook.md
 fi
 
-# cd ..
+cd ..
 
-cat `find *md` >> ./output/fullbook.md
+cat `find *md` >> output/fullbook.md
 
 # To Build additional kinds of ebooks, uncomment these lines and name your book.
 
 BOOKNAME="YOURGITTENBURGEBOOK"
 
-cd ./build
-pandoc -S --toc --epub-stylesheet=./build/style.css --epub-metadata=./output/metadata.xml -o ./output/$BOOKNAME.epub `find *md`
+pandoc -S --toc --epub-stylesheet=build/style.css --epub-metadata=build/metadata.xml -o output/$BOOKNAME.epub `find *md`
 
-pandoc -S --toc --epub-stylesheet=./build/style.css --epub-metadata=./output/metadata.xml -o ./output/$BOOKNAME.docx `find *md`
+pandoc -S --toc --epub-stylesheet=build/style.css --epub-metadata=build/metadata.xml -o output/$BOOKNAME.docx `find *md`
 
-pandoc -o ./output/$BOOKNAME.html `find *md`
+pandoc -o output/$BOOKNAME.html `find *md`

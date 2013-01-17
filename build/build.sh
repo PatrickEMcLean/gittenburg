286e5d642c695b655ab33b8d3d489412c285438d
PatrickEMcLean <patrickemclean@gmail.com>
2013-01-17 17:40:26 -0500

    Update build/build.sh

--- build/build.sh
+++ build/build.sh
@@ -17,10 +17,10 @@ cat `find *md` >> output/fullbook.md
 
 # To Build additional kinds of ebooks, uncomment these lines and name your book.
 
-BOOKNAME="YOURGITTENBURGEBOOK"
+# BOOKNAME="YOURGITTENBURGEBOOK"
 
-pandoc -S --toc --epub-stylesheet=build/style.css --epub-metadata=build/metadata.xml -o output/$BOOKNAME.epub `find *md`
+# pandoc -S --toc --epub-stylesheet=build/style.css --epub-metadata=build/metadata.xml -o output/$BOOKNAME.epub `find *md`
 
-pandoc -S --toc --epub-stylesheet=build/style.css --epub-metadata=build/metadata.xml -o output/$BOOKNAME.docx `find *md`
+# pandoc -S --toc --epub-stylesheet=build/style.css --epub-metadata=build/metadata.xml -o output/$BOOKNAME.docx `find *md`
 
-pandoc -o output/$BOOKNAME.html `find *md`
+# pandoc -o output/$BOOKNAME.html `find *md`
